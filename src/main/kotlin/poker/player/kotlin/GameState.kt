764d88b0a8d0c23263813693b2c22d113f70389a
Maciej Choluj 🐧 <maciej.choluj@getbring.com>
2025-09-10 09:17:09 +0200

    [api] creates GameState object

---
+++ src/main/kotlin/poker/player/kotlin/GameState.kt
@@ -0,0 +1,32 @@
+package poker.player.kotlin
+
+data class GameState(
+    val tournament_id: String,
+    val game_id: String,
+    val round: Int,
+    val bet_index: Int,
+    val small_blind: Int,
+    val current_buy_in: Int,
+    val pot: Int,
+    val minimum_raise: Int,
+    val dealer: Int,
+    val orbits: Int,
+    val in_action: Int,
+    val players: List<Player>,
+    val community_cards: List<Card>
+)
+
+data class Player(
+    val id: Int,
+    val name: String,
+    val status: String,
+    val version: String,
+    val stack: Int,
+    val bet: Int,
+    val hole_cards: List<Card>? = null  // Nullable for players without hole cards
+)
+
+data class Card(
+    val rank: String,
+    val suit: String
+)

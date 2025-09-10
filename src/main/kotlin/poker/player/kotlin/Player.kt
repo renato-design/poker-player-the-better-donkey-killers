f0dab9f77fc6aefa173814a971b38a5ec47bd5b3
Maciej Choluj 🐧 <maciej.choluj@getbring.com>
2025-09-10 14:24:44 +0200

    Revert "[api] checks for figures"
    This reverts commit 8d66de6ce90731dd09f7f6917d9a62366c129570.

--- src/main/kotlin/poker/player/kotlin/Player.kt
+++ src/main/kotlin/poker/player/kotlin/Player.kt
@@ -32,24 +32,21 @@ fun makeBet(gameState: GameState): Int {
 
     val handType = HandEvaluator().evaluateHand(flop.plus(myCards))
 
-    val raiseAmount = if (flop.size == 0 ) {
-        gameState.minimum_raise
-    } else {
-        val flopAdjustment = when (HandEvaluator().evaluateHand(flop)) {
-            HandType.HIGH_CARD -> 0.0
-            HandType.PAIR -> 0.2
-            HandType.TWO_PAIR -> 0.4
-            HandType.THREE_OF_A_KIND -> 0.6
-            HandType.STRAIGHT -> 0.8
-            HandType.FLUSH -> 0.8
-            HandType.FULL_HOUSE -> 1.0
-            HandType.FOUR_OF_A_KIND -> 1.0
-            HandType.STRAIGHT_FLUSH -> 1.0
-            HandType.ROYAL_FLUSH -> 1.0
-        }
-
-        (gameState.minimum_raise * (flopAdjustment)).toInt()
+    val flopAdjustment = when (HandEvaluator().evaluateHand(flop)) {
+        HandType.HIGH_CARD -> 0.0
+        HandType.PAIR -> 0.2
+        HandType.TWO_PAIR -> 0.4
+        HandType.THREE_OF_A_KIND-> 0.6
+        HandType.STRAIGHT-> 0.8
+        HandType.FLUSH -> 0.8
+        HandType.FULL_HOUSE -> 1.0
+        HandType.FOUR_OF_A_KIND -> 1.0
+        HandType.STRAIGHT_FLUSH-> 1.0
+        HandType.ROYAL_FLUSH -> 1.0
     }
+
+    val raiseAmount = (gameState.minimum_raise * (flopAdjustment)).toInt()
+
 
     if(flop.size < 3 ) {
         if (requiredCall <= 0) {

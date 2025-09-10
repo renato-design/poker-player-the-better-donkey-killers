7224f7983d119a5764af1f11db93e6def0e902d8
Maciej Choluj 🐧 <maciej.choluj@getbring.com>
2025-09-10 12:39:58 +0200

    [api] checks for figures

--- src/main/kotlin/poker/player/kotlin/Player.kt
+++ src/main/kotlin/poker/player/kotlin/Player.kt
@@ -70,8 +70,8 @@ fun makeBet(gameState: GameState): Int {
         HandType.HIGH_CARD -> 0
         HandType.PAIR -> 0
         HandType.TWO_PAIR -> requiredCall
-        HandType.THREE_OF_A_KIND-> requiredCall + (gameState.minimum_raise * 0.5).toInt()
-        HandType.STRAIGHT-> requiredCall + (gameState.minimum_raise * 1).toInt()
+        HandType.THREE_OF_A_KIND-> requiredCall + (gameState.minimum_raise * 1).toInt()
+        HandType.STRAIGHT-> requiredCall + (gameState.minimum_raise * 1.5).toInt()
         HandType.FLUSH -> requiredCall + gameState.minimum_raise * 2
         HandType.FULL_HOUSE -> requiredCall + gameState.minimum_raise * 2
         HandType.FOUR_OF_A_KIND,
